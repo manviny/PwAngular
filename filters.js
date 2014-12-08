57350633c8b3f65556a5ebc18427c32c02560860
manviny <manol@manol.home>
2014-12-08 10:06:53 +0100

    FILTERS

---
+++ filters.js
@@ -0,0 +1,31 @@
+
+	/**
+	 * Useful javascript
+	 */
+
+	// return number of words
+	// string.split(' ', 8).join(' ')
+
+
+	/**
+	 * trust HTML will output html as it is
+	 * <div ng-bind-html="htmlString | unsafe "></div>
+	 * @return HTML
+	 */
+	app.filter('unsafe', function($sce) {
+	    return function(val) {
+	        return $sce.trustAsHtml(val);
+	    };
+	});
+
+
+	/**
+	 * Splits a string into an array given the sepatator ( | , ' ' ) and returns the desired index
+	 * [imagen1.jpg|imagen2.jpg|imagen3.jpg]
+	 * @return {{lit.images | split:'|':1}} -> imagen2.jpg
+	 */
+    app.filter('split', function() {
+        return function(input, splitChar, splitIndex) {
+            return input.split(splitChar)[splitIndex];
+        };
+    });
