0e17eb84111efa553430431b31b6f136888e92d9
manviny <manol@manol.home>
2014-12-14 17:03:15 +0100

    added filters to services

--- pwservice.js
+++ pwservice.js
@@ -1,7 +1,7 @@
 app.factory('pw', function ($q, $http) {
     // Service logic
-    console.debug("MODULO","cargado desde pw");
-    
+    console.debug("MODULO","cargado desde pw1");
+
     /**
      * numChildren(1014)
      * @type {pageId}
@@ -48,3 +48,29 @@ app.factory('pw', function ($q, $http) {
 
 
   });
+
+  
+  /**
+   * trust HTML will output html as it is
+   * <div ng-bind-html="htmlString | unsafe "></div>
+   * @return HTML
+   */
+  app.filter('unsafe', function($sce) {
+      return function(val) {
+          return $sce.trustAsHtml(val);
+      };
+  });
+
+
+  /**
+   * Splits a string into an array given the sepatator ( | , ' ' ) and returns the desired index
+   * [imagen1.jpg|imagen2.jpg|imagen3.jpg]
+   * @return {{lit.images | split:'|':1}} -> imagen2.jpg
+   */
+    app.filter('split', function() {
+        return function(input, splitChar, splitIndex) {
+            return input.split(splitChar)[splitIndex];
+        };
+    });
+
+
